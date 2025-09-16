e743d9c506b34ddeb603fc35c063c427abbfcb01
Dan Moffat <danmoffat@gmail.com>
2025-09-16 17:03:11 +0100

    Message formatting.

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,9 +1,12 @@
 import {Args, Command, Flags} from '@oclif/core'
+import fs from 'node:fs'
 
 export default class Add extends Command {
   static override args = {
     message: Args.string({description: 'Message to add to journal', required: true}),
   }
+  static override description = 'Add entries to the journal'
+  static override examples = ['<%= config.bin %> <%= command.id %>']
   static override flags = {
     confidence: Flags.string({description: 'Confidence level from 1- 5', required: true}),
   }
---
+++ src/commands/list.ts
@@ -0,0 +1,10 @@
+import {Command} from '@oclif/core'
+
+export default class List extends Command {
+  static override description = 'List all journal entries'
+  static override examples = ['<%= config.bin %> <%= command.id %>']
+
+  public async run(): Promise<void> {
+    this.log('Here you can see all your journal entries...')
+  }
+}
