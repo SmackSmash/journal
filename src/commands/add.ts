d7991c7c29509b0093c2558e54263b9b887a1b8a
Dan Moffat <danmoffat@gmail.com>
2025-09-16 16:35:47 +0100

    Message formatting

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -2,16 +2,16 @@ import {Args, Command, Flags} from '@oclif/core'
 
 export default class Add extends Command {
   static override args = {
-    message: Args.string({description: 'Message to add to journal'}),
+    message: Args.string({description: 'Message to add to journal', required: true}),
   }
   static override flags = {
-    confidence: Flags.string({description: 'Confidence level from 1- 5'}),
+    confidence: Flags.string({description: 'Confidence level from 1- 5', required: true}),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Add)
 
     this.log(args.message)
-    this.log(flags.confidence)
+    this.log(`# → Entry added on ${new Date().toISOString().split('T')[0]} with confidence level ${flags.confidence}/5`)
   }
 }
